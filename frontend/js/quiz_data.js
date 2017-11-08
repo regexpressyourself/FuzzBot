ef409bb551cf7febe6f4bdd1211f6c94deecece4
Sam Messina <sam@smessina.com>
2017-11-08 14:30:09 -0600

    make some sample quiz data
    https://www.youtube.com/watch?v=c8HG6QiEIso

---
+++ frontend/js/quiz_data.js
@@ -0,0 +1,14 @@
+let quiz_questions = {
+    questions: [
+        {
+            q: "What is the best war to do?",
+            a: [
+                "North Korea",
+                "Russia",
+                "Syria",
+                "China",
+            ],
+            correct: "North Korea"
+        }
+    ]
+};
