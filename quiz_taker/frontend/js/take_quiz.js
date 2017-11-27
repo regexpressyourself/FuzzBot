d85d1e01085ba85e117e99bfc0d2ec4624ca4610
Sam <samuel.messina@siu.edu>
2017-11-27 12:41:34 -0600

    basic way to query against existing answer data when answering questions

--- quiz_taker/frontend/js/take_quiz.js
+++ quiz_taker/frontend/js/take_quiz.js
@@ -3,6 +3,17 @@ function sleep (time) {
 }
 let answers = JSON.parse(httpGet("/api/get_answers"));
 
+let checkAnswers = (q) => {
+    let i = 0;
+    for (let ans of answers) {
+        if (q == ans["q"]){
+            return i;
+        }
+        i++;
+    }
+    return -1;
+}
+
 let selectAnswer = (q_num) => {
     // get the question element/text
     let q_id = "q"+q_num;
@@ -22,13 +33,37 @@ let selectAnswer = (q_num) => {
     // iterate through the answers, making an array to query against
     for (let i = 0; i < num_answers; i++) {
         let a_id = q_id+"-"+i;
-        a.push(document.getElementById(q_id+"-"+i).value);
+        a.push(document.getElementById(q_id+"-"+i).value.substring(3));
     }
 
-    // get a random answer number 
-    let ans_num  = Math.floor(Math.random() * (num_answers - 0)); 
+    let ans_index = checkAnswers(q);
+    let prev_ans;
+    let ans_num;
+    if (ans_index >= 0) {
+        prev_ans = answers[ans_index];
+        if (prev_ans["correct"]) {
+            ans_num = a.indexOf(prev_ans["correct"]);
+        }
+        else {
+            let i = 0;
+            for (let ans of a) {
+                if (prev_ans["guessed"].indexOf(ans) < 0) {
+                    console.log(prev_ans);
+                    console.log(i);
+                    ans_num = i;
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+    else {
+        // get a random answer number 
+        ans_num  = Math.floor(Math.random() * (num_answers - 0)); 
+    }
 
     // select that answer
+    console.log(q_id+"-"+ans_num);
     document.getElementById(q_id+"-"+ans_num).checked = true;
 }
 
