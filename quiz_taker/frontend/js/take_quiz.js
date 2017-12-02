19b917a23b7f9d53b4aa63300366f6659d091130
Sam <samuel.messina@siu.edu>
2017-12-02 09:37:41 -0600

    refactor take_quiz to use the bot image

--- quiz_taker/frontend/js/take_quiz.js
+++ quiz_taker/frontend/js/take_quiz.js
@@ -1,16 +1,16 @@
-function sleep (time) {
+function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve, time));
 }
-let answers = JSON.parse(httpGet("/api/get_answers"));
+let previous_attempt_data = JSON.parse(httpGet("/api/get_answers"));
 
 let checkAnswers = (q) => {
     let i = 0;
     let answer_map = {};
-    for (let ans of answers) {
+    for (let ans of previous_attempt_data) {
         // TODO: change this to a fuzzy check
         // Perhaps iterate through all answers and select the 
         // "closest" one
-        if (q == ans["q"]){
+        if (q == ans["q"]) {
             return i;
         }
         i++;
@@ -20,67 +20,124 @@ let checkAnswers = (q) => {
 
 let selectAnswer = (q_num) => {
     // get the question element/text
-    let q_id = "q"+q_num;
+    let q_id = "q" + q_num;
     let q = document.getElementById(q_id);
 
-    if (!q) {return;}
-    else { 
-        q.scrollIntoView({ behavior: 'smooth' });
+    if (!q) {
+        return;
+    } else {
+        q.scrollIntoView({
+            behavior: 'smooth'
+        });
         q = q.innerHTML;
     }
 
     // get the answer elements
-    let a = [];
-    let num_answers = document.getElementById(q_id+"_answers").children.length; 
-    if (num_answers > 4) {return;}
+    let answers = [];
+    let num_answers = document.getElementById(q_id + "_answers").children.length;
+    if (num_answers > 4) {
+        return;
+    }
 
     // iterate through the answers, making an array to query against
     for (let i = 0; i < num_answers; i++) {
-        let a_id = q_id+"-"+i;
-        a.push(document.getElementById(q_id+"-"+i).value.substring(3));
+        let a_id = q_id + "-" + i;
+        answers.push(document.getElementById(q_id + "-" + i).value.substring(3));
     }
 
     // select an answer
     let ans_index = checkAnswers(q); // get the index of the answer data
-    let prev_ans; // the answer data from previous attempts
+    let known_q_data; // the answer data from previous attempts
     let ans_num; // the number of the answer to be guessed
 
     // if we've seen the question before...
     if (ans_index >= 0) {
-        prev_ans = answers[ans_index];
+        known_q_data = previous_attempt_data[ans_index];
         // guess the correct answer if we know it
-        if (prev_ans["correct"]) {
-            ans_num = a.indexOf(prev_ans["correct"]);
+        if (known_q_data["correct"]) {
+            ans_num = answers.indexOf(known_q_data["correct"]);
         }
         // otherwise, guess a previously-unguessed answer
-        // TODO: optimize the random guesses here
         else {
-            for (let i = 0; i < a.length; i++ ) {
-                let ans = a[i];
-                if (prev_ans["guessed"].indexOf(ans) < 0) {
-                    ans_num = i;
-                    break;
-                }
-            }
+            ans_num = guessUnguessedAnswer(known_q_data, answers);
         }
     }
     // if we haven't seen the answer before, guess randomly
-    // TODO: optimize random guesses here
     else {
-        // get a random answer number 
-        ans_num  = Math.floor(Math.random() * (num_answers - 0)); 
+        ans_num = naiveGuess(num_answers);
     }
-
     // select the answer at ans_num
-    document.getElementById(q_id+"-"+ans_num).checked = true;
+    document.getElementById(q_id + "-" + ans_num).checked = true;
 }
 
-let takeTest = (json_data) => 
-{
+let naiveGuess = (num_answers) => {
+    // TODO: optimize the random guesses here
+    return Math.floor(Math.random() * num_answers);
+}
+
+let guessUnguessedAnswer = (known_q_data, answers) => {
+    // TODO: optimize the educated guesses here
+    let ans_num = -1;
+    for (let i = 0; i < answers.length; i++) {
+        let ans = answers[i];
+        if (known_q_data["guessed"].indexOf(ans) < 0) {
+            ans_num = i;
+            break;
+        }
+    }
+    return ans_num;
+}
+
+let changeRobotCaption = () => {
+    let robot_captions = ["Woo! This one's tough",
+        "Oops, didn't study that!",
+        "Maybe I shouldn't have skipped class...",
+        "Did we ever talk about this?",
+        "If I can get a C, I'll be happy",
+        "No one cares about GPA anyway, right?",
+        "Sure hope there's extra credit",
+        "That's it, I'm going to office hours",
+        "Wait, we needed to know that?"
+    ];
+    return robot_captions[Math.floor(Math.random() * robot_captions.length)];
+}
+
+let botStartTest = (robot_image, robot_chat) => {
+    robot_image.style.position = "fixed";
+    robot_image.style.top = "50%";
+
+    robot_chat.className = "top-chat";
+    return 1;
+}
+
+let botCompleteTest = (robot_image, robot_chat) => {
+    robot_image.style.bottom = "0px";
+    robot_image.style.top = "initial";
+
+    robot_chat.className = "side-chat";
+    robot_chat.innerText = "Let's see how we did!";
+    return 1;
+}
+
+let takeTest = (json_data) => {
+    let robot_image = document.getElementById("bot-image");
+    let robot_chat = document.getElementById("bot-chat");
+
+    botStartTest(robot_image, robot_chat);
     for (let q_num of Array(20).keys()) {
         // wait a bit
-        sleep(300*q_num).then(() => {selectAnswer(q_num)})
+        if (q_num % 3 === 0 && q_num < 17) {
+            sleep(300 * q_num).then(() => {
+                selectAnswer(q_num);
+                robot_chat.innerText = changeRobotCaption();
+            })
+        } else {
+            sleep(300 * q_num).then(() => {
+                selectAnswer(q_num)
+            })
+        }
     }
+    sleep(300 * 20).then(() => {
+        botCompleteTest(robot_image, robot_chat)
+    })
 }
-
-
