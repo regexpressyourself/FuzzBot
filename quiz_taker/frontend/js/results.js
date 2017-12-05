6a0746522fbbc349ac5d094758ae7fc38e3cbb28
Sam <samuel.messina@siu.edu>
2017-12-05 16:33:43 -0600

    put a historical graph on the results page

--- quiz_taker/frontend/js/results.js
+++ quiz_taker/frontend/js/results.js
@@ -12,7 +12,12 @@ let correct = results["num_correct"]/20*100 + "%";
 
 let num_guessed = results["num_guessed"];
 
-num_guessed_correctly = results["num_guessed_correctly"]/num_guessed*100 
+let num_guessed_correctly = results["num_guessed_correctly"]/num_guessed*100 
+
+let guessed_hist = results["guessed_hist"];
+let guessed_correctly_hist = results["guessed_correctly_hist"];
+let correct_hist= results["correct_hist"];
+
 if (isNaN(num_guessed_correctly)) {
     num_guessed_correctly = "N/A";
 }
@@ -23,4 +28,27 @@ else {
 document.getElementById("total-grade").innerHTML = correct;
 document.getElementById("num-guessed").innerHTML = num_guessed;
 document.getElementById("guessed-correct").innerHTML = num_guessed_correctly;
+let ctx = document.getElementById('result-chart').getContext('2d');
 
+let labels = [...Array(guessed_hist.length).keys()];
+
+let myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: labels,
+        datasets: [
+            { 
+                data: correct_hist,
+                label: "Correct",
+                borderColor: "green",
+                fill: false
+            },
+            { 
+                data: guessed_hist,
+                label: "Guessed",
+                borderColor: "red",
+                fill: false
+            }
+        ]}
+
+});
